e6f99abb35893883f632fb5bac2fef379168fae0
savi4m83-crypto <savi4m83@gmail.com>
2026-02-13 18:55:43 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#There are five trees in Jack's front yard.
+# He checks each tree to find out how tall it is in inches and writes the height
+# on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average 
+# height of a tree in Jack's front yard?
+
+num1=int(input("Enter your num1:"))
+num2=int(input("Enter your num2:"))
+num3=int(input("Enter your num3:"))
+num4=int(input("Enter your num4:"))
+num5=int(input("Enter your num5:"))
+print((num1+num2+num3+num4+num5)/5)
